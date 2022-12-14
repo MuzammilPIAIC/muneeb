1b802c10bdca52cab659bc6012672ef41db31764
MuzammilPIAIC <50516983+MuzammilPIAIC@users.noreply.github.com>
2022-12-14 12:29:59 +0500

    Update settings.py

--- record/settings.py
+++ record/settings.py
@@ -20,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config(
-    "SECRET_KEY",
-    default="django-insecure-1lmyw6s4jb29#-f3oxx!)yx=_7fty-cxw2h4w4$tpm1e+pje#y",
-)
-
+# SECRET_KEY = config(
+#     "SECRET_KEY",
+#     default="django-insecure-1lmyw6s4jb29#-f3oxx!)yx=_7fty-cxw2h4w4$tpm1e+pje#y",
+# )
+SECRET_KEY = 'usjl%evumbsj=199&+=_e9_=n@s+d99303xv8co9b^x)3pd%_a_*h*8cgsr(n$2aiyr=^_m_tnu2ndybaehg9>'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
